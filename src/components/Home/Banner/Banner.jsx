7ccc19dbf89cc4d640a6e7ddca4998bd4ebb9054
rafimd422 <rafimf2222@gmail.com>
2023-09-24 16:59:51 +0600

    added google font

--- src/components/Home/Banner/Banner.jsx
+++ src/components/Home/Banner/Banner.jsx
@@ -5,7 +5,7 @@ const Banner = () => {
 
   return (
 <>
-<section className="xl:h-[60vh] h-fit xl:justify-around justify-center flex md:gap-2 flex-wrap gap-6">
+<section className="xl:h-[60vh] h-fit xl:justify-around justify-center flex md:gap-2 flex-wrap gap-6 bg-banner">
 <div className="flex flex-col gap-2 self-center md:p-0 p-4">
 <h2 className="text-zinc-900 text-6xl font-extrabold ">One Step <br />Closer To Your <br /> <span className="text-indigo-400 leading-10">Dream Job</span></h2>
 <p class=" text-neutral-500 text-lg">Explore thousands of job opportunities with all the <br />information you need. Its your future. Come find it. Manage all <br />your job application from start to finish.</p>
